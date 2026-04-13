76e03b6c2667c9316b01bbd3d21f324c00729b7e
rudibarani <7797301+rudibarani@users.noreply.github.com>
2026-04-13 11:42:57 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,10 @@
+DOMAIN = "ha-energy-share"
+
+CONF_SOURCES = "sources"
+CONF_LOAD = "load"
+CONF_CONSUMERS = "consumers"
+CONF_PRIORITY = "priority"
+CONF_DEADBAND = "deadband"
+
+DEFAULT_DEADBAND = 50
+UPDATE_INTERVAL = 10
